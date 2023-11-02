4bec0f5b952f5681d4e2cdeeecbe9f9382f9f376
Maaz Ahmed <elfilosof@protonmail.com>
2023-11-02 02:53:41 -0700

    external api use

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
-// Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Define a route for '/about'
-app.get('/about', (req, res) => {
-  res.send('About Us');
+app.get('/users', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    const users = response.data;
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to fetch data from the API' });
+  }
 });
 
-// Define a route with a dynamic parameter
-app.get('/user/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`User Profile for ID ${userId}`);
-});
-
-// Define a route for POST requests
-app.post('/create', (req, res) => {
-  // Handle creating a new resource
-  res.send('Resource created successfully');
-});
-
-// Define a route for PUT requests
-app.put('/update/:id', (req, res) => {
-  const resourceId = req.params.id;
-  // Handle updating the resource with the given ID
-  res.send(`Resource with ID ${resourceId} updated`);
-});
-
-// Define a route for DELETE requests
-app.delete('/delete/:id', (req, res) => {
-  const resourceId = req.params.id;
-  // Handle deleting the resource with the given ID
-  res.send(`Resource with ID ${resourceId} deleted`);
-});
-
-// Start the server on port 8080
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
