c60914bfaecb31994e94e3c2845b521331494ce4
Maaz Ahmed <elfilosof@protonmail.com>
2023-11-02 03:11:39 -0700

    pokemon api

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ app.get('/users', async (req, res) => {
   }
 });
 
+// get api for pokemon api https://pokeapi.co/
+
+app.get('/pokemon', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://pokeapi.co/api/v2/pokemon?limit=100'
+    );
+    const pokemon = response.data;
+    res.json(pokemon);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to fetch data from the API' });
+  }
+});
+
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
